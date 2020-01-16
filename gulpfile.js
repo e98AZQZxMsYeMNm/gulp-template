e62230406c54de0a24d00427f5abc953a43f35b6
takegami <t.takegami@terminal-system.biz>
2020-01-16 09:26:24 +0900

    画像の自動生成対応

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ var htmlmin = require('gulp-htmlmin');
 var cleanCSS = require("gulp-clean-css");
 var connect = require('gulp-connect');
 var nunjucksRender = require('gulp-nunjucks-render');
+var imagemin = require('gulp-imagemin');
+var mozjpeg = require('imagemin-mozjpeg');
+var pngquant = require('imagemin-pngquant');
 
 
 // 監視　※gulp4の書き方です。
@@ -14,6 +17,7 @@ gulp.task( "watch", () => {
     gulp.watch( "src/sass/**/*.scss", gulp.series( "sass" ) ); // sassディレクトリ以下の.scssファイルの更新を監視
     gulp.watch( "src/njk/**/*.njk", gulp.series( "njk" ) ); // njkディレクトリ以下の.njkファイルの更新を監視
     gulp.watch( "src/js/**/*.js", gulp.series( "js" ) );
+    gulp.watch( "src/img/**/*.{jpg,jpeg,png,gif,svg}", gulp.series('img'));
     gulp.parallel('serve');
 });
 
@@ -53,6 +57,21 @@ gulp.task('js', () => {
         .pipe(rename({extname: '.min.js'}))
         .pipe(gulp.dest('./public/js/'));
 });
+
+gulp.task('img', function () {
+  gulp.src('./src/img/**/*.{jpg,jpeg,png,gif,svg}')
+  .pipe(imagemin(
+    [
+      pngquant({ quality: '65-80', speed: 1 }),
+      mozjpeg({ quality: 80 }),
+      imagemin.svgo(),
+      imagemin.gifsicle()
+    ]
+  ))
+  .pipe(gulp.dest('./public/img/'));
+});
+
+
 
 gulp.task('default', gulp.series(gulp.parallel('serve', 'watch'),function(){
 }));
