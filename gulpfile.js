1b0ae5e74377aac2e5b5681a476c80e21190c63b
takegami <t.takegami@terminal-system.biz>
2020-02-06 09:51:57 +0900

    自動更新を追加

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,19 @@ var imagemin = require('gulp-imagemin');
 var mozjpeg = require('imagemin-mozjpeg');
 var pngquant = require('imagemin-pngquant');
 
+const webpackStream = require("webpack-stream");
+const webpack = require("webpack");
+const webpackConfig = require("./webpack.config");
+
 
 // 監視　※gulp4の書き方です。
 gulp.task( "watch", () => {
     gulp.watch( "src/sass/**/*.scss", gulp.series( "sass" ) ); // sassディレクトリ以下の.scssファイルの更新を監視
     gulp.watch( "src/njk/**/*.njk", gulp.series( "njk" ) ); // njkディレクトリ以下の.njkファイルの更新を監視
-    gulp.watch( "src/js/**/*.js", gulp.series( "js" ) );
-    gulp.watch( "src/img/**/*.{jpg,jpeg,png,gif,svg}", gulp.series('img'));
+    gulp.watch( "src/js/main.js", gulp.series('bundle')); // webpack用のデータ
+    gulp.watch( "src/js/**/*.js", gulp.series( "js" ) ); // js出力
+    gulp.watch( "src/lib/**/*", gulp.series("lib")); // ライブラリなどを直接出力
+    gulp.watch( "src/img/**/*.{jpg,jpeg,png,gif,svg}", gulp.series('img')); // 画像ファイルを自動調整
     gulp.parallel('serve');
 });
 
@@ -25,7 +31,8 @@ gulp.task( "watch", () => {
 gulp.task('serve', () => {
   connect.server({
     root: './public',
-    livereload: true
+    livereload: true,
+	  host:'0.0.0.0'
   });
 });
 
@@ -37,7 +44,8 @@ gulp.task( "sass", () => {
         .pipe(autoprefixer())
         .pipe(cleanCSS())
         .pipe(rename({extname: '.min.css'}))
-        .pipe(gulp.dest( './public/css' ));
+        .pipe(gulp.dest( './public/css' ))
+        .pipe(connect.reload());
 });
 
 // njk
@@ -48,32 +56,47 @@ gulp.task( "njk",  () => {
         }))
         .pipe(htmlmin({collapseWhitespace : true, removeComments : true}))
         .pipe(rename({extname: '.html' }))
-        .pipe( gulp.dest( "./public/" ) );
+        .pipe( gulp.dest( "./public/" ) )
+        .pipe(connect.reload());
 });
 
 gulp.task('js', () => {
-    return gulp.src(['./src/js/*.js', '!./js/*.min.js'])
+  return gulp.src(['./src/js/*.js', '!./js/*.min.js', '!./js/main.js'])
         .pipe(uglify())
         .pipe(rename({extname: '.min.js'}))
         .pipe(gulp.dest('./public/js/'));
 });
 
+
+gulp.task('lib', () => {
+  return gulp.src(['./src/lib/**/*'])
+        .pipe(gulp.dest('./public/lib/'))
+        .pipe(connect.reload());
+});
+
+
+
+gulp.task('bundle', () => {
+  return webpackStream(webpackConfig, webpack)
+    .pipe(gulp.dest("public/js"))
+    .pipe(connect.reload());
+});
+
 gulp.task('img', function () {
-    return gulp.src('./src/img/**/*.{jpg,jpeg,png,gif,svg}')
+  return gulp.src('./src/img/**/*.{jpg,jpeg,png,gif,svg}')
         .pipe(imagemin(
           [
-            pngquant({ quality: '65-80', speed: 1 }),
+            pngquant({ quality: [.7, .85], speed: 1 }),
             mozjpeg({ quality: 80 }),
             imagemin.svgo(),
             imagemin.gifsicle()
           ]
         ))
-        .pipe(gulp.dest('./public/img/'));
+        .pipe(gulp.dest('./public/img/'))
+        .pipe(connect.reload());
 });
-
-
 
 gulp.task('default', gulp.series(gulp.parallel('serve', 'watch'),function(){
 }));
 
-gulp.task('build', gulp.series(gulp.parallel('njk', 'js', 'img', 'sass')));
+gulp.task('build', gulp.series(gulp.parallel('njk', 'sass', 'js', 'img', 'lib', 'bundle')));
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // モード値を production に設定すると最適化された状態で、
+    // development に設定するとソースマップ有効でJSファイルが出力される
+    mode: "production",
+    externals: [
+        {
+          jquery: 'jQuery'
+        }
+    ],
+    // メインのJS
+    entry: "./src/js/main.js",
+    // 出力ファイル
+    output: {
+      filename: "bundle.js"
+    }
+}
